975ae1240b8b45b7d83e1fc3cece064bfb25582d
Thiago Vazquez <thgovazz@gmail.com>
2022-10-28 18:50:17 -0300

    Toda la segunda entra

--- archivosJs/etapa1.js
+++ archivosJs/etapa1.js
@@ -1,241 +1,155 @@
-//Presentacion
-let preguntaNombre = prompt("Ingrese su nombre")
-function saludo(nombre) {
-    console.log("¡Hola, ",nombre+"!.","Bienvenido/a a GeT in!.Tienda de ropa online")
+let nombre = prompt("Ingrese su nombre")
+function saludo(dato) {
+    console.log(`¡Hola, ${dato}!.Bienvenido/a a GeT in!.Tienda de ropa online`)
 }
-saludo(preguntaNombre)
-//funciones
+saludo(nombre)
+
+const listaProd = [
+    {id:1, nombre: "Buzo dino drew", talle:"M", precio: 20000, marca:"Drew", categoria:"Buzo",},
+    {id:2, nombre: "Buzo nike air", talle:"XXL", precio: 16000, marca:"Nike", categoria:"Buzo",},
+    {id:3, nombre: "Buzo adidas liso", talle:"S", precio: 15000, marca:"Adidas", categoria:"Buzo",},
+    {id:4, nombre: "Remera adidas blod", talle:"L", precio: 10000, marca:"Adidas", categoria:"Remera",},
+    {id:5, nombre: "Remera drew house", talle:"XL", precio: 15000, marca: "Drew", categoria:"Remera",},
+    {id:6, nombre: "Remera jordan 23", talle:"M", precio: 11000, marca:"Nike", categoria:"Remera",}
+]
+
+const carrito = []
+
 let resultado
-function opcion() {
-  resultado = Number(prompt("Elija una opción"))
-}
-//modelos de zapatillas 
-const nike1 = {
-    modelo: "Nike Air Max 90",
-    precio: 26.500,
-    color: "Blanco"
-}
-const nike2 = {
-    modelo: "Nike Huarache",
-    precio: 28.000 ,
-    color: "Negro"
-}
-const nike3 = {
-    modelo: "Nike Air Force",
-    precio: 32.000,
-    color: "Blanco"
-}
-const nike4 = {
-    modelo: "Blazer Low 77",
-    precio: 32.000,
-    color: "Negro"
-}
-//------------------------------------
-//Modelos de buzo
-const buzo1 = {
-    modelo: "Buzo con estampado Adidas",
-    precio: 16.000,
-    color: "Azul" 
-}
-const buzo2 = {
-    modelo: "Buzo con estampado Nike",
-    precio: 17.000,
-    color: "Marron" 
-}
-const buzo3 = {
-    modelo: "Buzo Rip Curl",
-    precio: 14.000,
-    color: "Negro"
-}
-const buzo4 = {
-    modelo: "Buzo Vans",
-    precio: 14.000,
-    color: "Blanco"
+function opcion(){
+    resultado = Number(prompt("Elija una opcion"))
 }
 
-
-
-function buzos() {
-    console.log("¿Que modelo de buzos desea mirar?")
-    console.log("1- Con capucha")
-    console.log("2- Sin capucha")
+function remeras() {
+    let remerasFilt = listaProd.filter((el)=> el.categoria == "Remera")
+    console.log("Estas son las remeras disponibles:")
+    remerasFilt.forEach((el) => console.log(`${el.id}- ${el.nombre}`))
     opcion()
+    console.log(resultado)
     switch (resultado) {
-        case 1:
-            console.log("De momento tenemos estos modelos:")
-            console.log("1- Con estampado")
-            console.log("Proximamente mas modelos")
-            console.log("0- Volver")
+        case 4:
+            console.log(`${remerasFilt[0].nombre} sale ${remerasFilt[0].precio} y su talle es ${remerasFilt[0].talle}`)
+            console.log("¿Desea comprarla?")
+            console.log("1- Si")
+            console.log("2- No")
             opcion()
-            if(resultado = 1){
-                console.log("En stock se encuentra:")
-                console.log(buzo1.modelo)
-                console.log("El precio es", buzo1.precio.toFixed(3),"y su color es",buzo1.color)
-                console.log("¿Desea comprarlo?")
-                console.log("1- Si")
-                console.log("2- No")
-                opcion()
-                if(resultado == 1){
-                    console.log("¡Gracias por su compra!")
-                }else if(resultado == 2){
-                    console.log("No hay problema, la proxima sera.")
-                }else{
-                    console.log("Error")
-                }
-            }else if(resultado == 0){
-                buzos()
-            }else{
-                console.log("Error")
+            if (resultado == 1) {
+                carrito.push(remerasFilt[0].precio)
+                console.log("Gracias por su compra! Vuelva pronto.")
+            }else if(resultado == 2){
+                console.log("No hay problema, la proxima sera.")
             }
             break;
-        case 2:
-            console.log("De momento tenemos estos modelos:")
-            console.log("1- Lisos")
-            console.log("Proximamente mas modelos")
-            console.log("0- Volver")
+        case 5:
+            console.log(`${remerasFilt[1].nombre} sale ${remerasFilt[1].precio} y su talle es ${remerasFilt[1].talle}`)
+            console.log("¿Desea comprarla?")
+            console.log("1- Si")
+            console.log("2- No")
             opcion()
-            if(resultado == 1){
-                console.log("En stock se encuentra:")
-                console.log(buzo3.modelo)
-                console.log("El precio es", buzo3.precio.toFixed(3),"y su color es",buzo3.color)
-                console.log("¿Desea comprarlo?")
-                console.log("1- Si")
-                console.log("2- No")
-                opcion()
-                if(resultado == 1){
-                    console.log("¡Gracias por su compra!")
-                }else if(resultado == 2){
-                    console.log("No hay problema, la proxima sera.")
-                }else{
-                    console.log("Error")
-                }
-            }else if(resultado == 0){
-                buzos()
-            }else{
-                console.log("Error")
+            if (resultado == 1) {
+                carrito.push(remerasFilt[1].precio)
+                console.log("Gracias por su compra! Vuelva pronto.")
+            }else if(resultado == 2){
+                console.log("No hay problema, la proxima sera.")
             }
             break;
+        case 6:
+            console.log(`${remerasFilt[2].nombre} sale ${remerasFilt[2].precio} y su talle es ${remerasFilt[2].talle}`)
+            console.log("¿Desea comprarla?")
+            console.log("1- Si")
+            console.log("2- No")
+            opcion()
+            if (resultado == 1) {
+                carrito.push(remerasFilt[2].precio)
+                console.log("Gracias por su compra! Vuelva pronto.")
+            }else if(resultado == 2){
+                console.log("No hay problema, la proxima sera.")
+            }
+            break;      
         default:
+            console.log("Error, elija una de las opciones disponibles.")
+            remeras()
             break;
     }
 }
 
-//--Menú Principal--
+function buzos() {
+    let buzosFilt = listaProd.filter((el)=> el.categoria == "Buzo")
+    console.log("Estos son los buzos disponibles:")
+    buzosFilt.forEach((el) => console.log(`${el.id}- ${el.nombre}`))
+    opcion()
+    console.log(resultado)
+    switch (resultado) {
+        case 1:
+            console.log(`${buzosFilt[0].nombre} sale ${buzosFilt[0].precio} y su talle es ${buzosFilt[0].talle}`)
+            console.log("¿Desea comprarlo?")
+            console.log("1- Si")
+            console.log("2- No")
+            opcion()
+            if (resultado == 1) {
+                carrito.push(buzosFilt[0].precio)
+                console.log("Gracias por su compra! Vuelva pronto.")
+            }else if(resultado == 2){
+                console.log("No hay problema, la proxima sera.")
+            }
+            break;
+        case 2:
+            console.log(`${buzosFilt[1].nombre} sale ${buzosFilt[1].precio} y su talle es ${buzosFilt[1].talle}`)
+            console.log("¿Desea comprarlo?")
+            console.log("1- Si")
+            console.log("2- No")
+            opcion()
+            if (resultado == 1) {
+                carrito.push(buzosFilt[1].precio)
+                console.log("Gracias por su compra! Vuelva pronto.")
+            }else if(resultado == 2){
+                console.log("No hay problema, la proxima sera.")
+            }
+            break;
+        case 3:
+            console.log(`${buzosFilt[2].nombre} sale ${buzosFilt[2].precio} y su talle es ${buzosFilt[2].talle}`)
+            console.log("¿Desea comprarlo?")
+            console.log("1- Si")
+            console.log("2- No")
+            opcion()
+            if (resultado == 1) {
+                carrito.push(buzosFilt[2].precio)
+                console.log("Gracias por su compra! Vuelva pronto.")
+            }else if(resultado == 2){
+                console.log("No hay problema, la proxima sera.")
+            }
+            break;     
+        default:
+            console.log("Error, elija una de las opciones disponibles.")
+            buzos()
+            break;
+    }
+}
 do {
-    console.log("¿Que desea mirar?")
-    console.log("1- Zapatillas")
+    console.log("Que desea mirar")
+    console.log("1- Remeras")
     console.log("2- Buzos")
-    console.log("0- Salir del menú")
+    console.log("3- Terminar compra")
+    console.log("0- Finalizar programa")
     opcion()
     switch (resultado) {
         case 1:
-            zapatillas()
+            remeras()
             break;
         case 2:
             buzos()
-            break
+            break;
+        case 3:
+            let precioFinal = carrito.reduce((acumulador, elemento) => acumulador + elemento, 0)
+            console.log(`El total de su compra es: ${precioFinal}`)
+            break;
         case 0:
-            alert("Fin del programa")         
+            console.log("Programa finalizado.")
+            break;
         default:
+            console.log("Error, elija una de las opciones disponibles.")
             break;
     }
-    if(resultado > 2 || resultado < 0 ){
-        console.log("Error")
-    }
+
 } while (resultado !== 0);
 
-let respuesta
-function zapatillas() {
-    console.log("¿Que modelos de zapatillas desea mirar?")
-    console.log("1- Deportivas")
-    console.log("2- De vestir")
-    opcion()
-    switch (resultado) {
-        //deportivas
-        case 1:
-            console.log("De momento tenemos estos dos modelos:")
-            console.log("1-",nike1.modelo)
-            console.log("2-",nike2.modelo)
-            console.log("0- Volver")
-            opcion()
-            //especificaciones
-            if(resultado == 1){
-                console.log("Este modelo lo tenemos en color:", nike1.color)
-                console.log("El precio es:"+ nike1.precio.toFixed(3))
-                console.log("1- si")
-                console.log("2- no")
-                console.log("¿Desea comprarla?")
-                opcion()
-                if(resultado == 1){
-                    console.log("¡Gracias por su compra!")
-                }else if(resultado == 2){
-                    console.log("No hay problema, la proxima sera")
-                }else{
-                    console.log("Error")
-                }
-            //especificaciones    
-            }else if(resultado == 2){
-                console.log("Este modelo lo tenemos en color:", nike2.color)
-                console.log("El precio es:", nike2.precio.toFixed(3))
-                console.log("1- si")
-                console.log("2- no")
-                console.log("¿Desea comprarla?")
-                opcion()
-                if(resultado == 1){
-                    console.log("¡Gracias por su compra!")
-                }else if(resultado == 2){
-                    console.log("No hay problema, la proxima sera")
-                }
-                else{
-                    console.log("Erorr")
-                }         
-            }else if(resultado == 0){
-                zapatillas()
-            }    
-            else{
-                console.log("Error")
-            }
-            break;
-            //de vestir    
-        case 2:
-            console.log("De momento tenemos estos dos modelos:")
-            console.log("1-",nike3.modelo)
-            console.log("2-",nike4.modelo)
-            console.log("0- Volver")
-            opcion()
-            //especificaciones
-            if(resultado == 1){
-                console.log("Este modelo lo tenemos en color:", nike3.color)
-                console.log("El precio es:"+ nike3.precio.toFixed(3))
-                console.log("1- si")
-                console.log("2- no")
-                console.log("¿Desea comprarla?")
-                opcion()
-                if(resultado == 1){
-                    console.log("¡Gracias por su compra!")
-                }else if(resultado == 2){
-                    console.log("No hay problema, la proxima sera")
-                }
-                else{
-                    console.log("Erorr")
-                }    
-            }else if(resultado == 2){
-                console.log("Este modelo lo tenemos en color:", nike4.color)
-                console.log("El precio es:"+ nike4.precio.toFixed(3))
-                console.log("1- si")
-                console.log("2- no")
-                console.log("¿Desea comprarla?")
-                opcion()
-                if(resultado == 1){
-                    console.log("¡Gracias por su compra!")
-                }else if(resultado == 2){
-                    console.log("No hay problema, la proxima sera")
-                }else{
-                    console.log("Error")
-                }
-            }
-            break     
-        default:
-            break;
-    }
-}
